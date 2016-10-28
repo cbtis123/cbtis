0c2ee2e11a2183a6805cae6c748ff27d2bed2243
Kevin Jael Gamboa Fernandez <k3vinfdz@gmail.com>
2016-10-28 10:25:40 -0500

    tabla horarios

--- database/migrations/2016_10_22_164637_create_horarios_table.php
+++ database/migrations/2016_10_22_164637_create_horarios_table.php
@@ -17,13 +17,21 @@ class CreateHorariosTable extends Migration
             $table->increments('id');
             $table->integer('aula_id')->unsigned()->nullable();
             $table->integer('grupo_id')->unsigned();
-            $table->integer('hora_id')->unsigned();
             $table->integer('materia_id')->unsigned();
             $table->integer('profesor_id')->unsigned()->nullable();
+            $table->time('lunes_i')->nullable();
+            $table->time('lunes_f')->nullable();
+            $table->time('martes_i')->nullable();
+            $table->time('martes_f')->nullable();
+            $table->time('miercoles_i')->nullable();
+            $table->time('miercoles_f')->nullable();
+            $table->time('jueves_i')->nullable();
+            $table->time('jueves_f')->nullable();
+            $table->time('viernes_i')->nullable();
+            $table->time('viernes_f')->nullable();
 
             $table->foreign('aula_id')->references('id')->on('aulas')->onDelete('cascade');
             $table->foreign('grupo_id')->references('id')->on('grupos')->onDelete('cascade');
-            $table->foreign('hora_id')->references('id')->on('horas')->onDelete('cascade');
             $table->foreign('materia_id')->references('id')->on('materias')->onDelete('cascade');
             $table->foreign('profesor_id')->references('id')->on('profesores')->onDelete('cascade');
             $table->timestamps();
