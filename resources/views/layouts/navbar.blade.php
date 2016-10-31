88a22a86a2cf96adb2a990e599c8fb653b54ee2a
Kevin Jael Gamboa Fernandez <k3vinfdz@gmail.com>
2016-10-31 11:29:29 -0600

    navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -4,14 +4,14 @@
 	<p align="center" style="font-family:Consolas;">SISTEMA INTEGRAL</p>	
 		<ul>
 		  <li><a class="active" href="#home">INICIO</a></li>
-		  <li><a href="#news">ALUMNOS</a></li>
-		  <li><a href="#contact">PROFESORES</a></li>
-		  <li><a href="#about">MATERIAS ACADEMICAS</a></li>
-		  <li><a href="#about">SALON DE CLASES</a></li>
-		  <li><a href="#about">INGLES</a></li>
-		  <li><a href="#about">ESTADISTICAS</a></li>
-		  <li><a href="#about">INGLES</a></li>
-		  <li><a href="#about">INGLES</a></li>
+		  <li><a href="{{ url('/alumnos') }}">ALUMNOS</a></li>
+		  <li><a href="{{ url('/profesores') }}">PROFESORES</a></li>
+		  <li><a href="{{ url('/materias') }}">MATERIAS ACADEMICAS</a></li>
+		  <li><a href="{{ url('/aulas') }}">SALON DE CLASES</a></li>
+		  <li><a href="{{ url('/tutores') }}">TUTORES</a></li>
+		  <li><a href="{{ url('/especialidades') }}">ESPECIALIDADES</a></li>
+		  <li><a href="{{ url('/horarios') }}">HORARIO</a></li>
+		  <li><a href="{{ url('/calificaciones') }}">CALIFICACION</a></li>
 		</ul>	
 	</div>
 </nav>
